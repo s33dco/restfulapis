758c56405134381efd60b84932ae6e55881ee449
s33dco <code@s33d.co>
2019-01-30 12:58:05 +0000

    mongo validators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -6,12 +6,52 @@ mongoose.connect('mongodb://localhost/playground', { useNewUrlParser: true })
 
 
 const courseSchema = new mongoose.Schema({
-  name : String,
+  name : {
+            type: String,
+            required: true
+          },
+
+  category: {
+            type: String,
+            enum :['web', 'mobile', 'newtwork'],
+            // uppercase: true,
+            lowercase: true,
+            trim : true
+            },
+
   author: String,
-  tags : [String],
-  date: {type: Date, default: Date.now},
-  isPublished: Boolean
-});
+
+  tags : {
+          type: Array,
+          validate: {                   // custom validator
+            isAsync: true,              // async - isAsync
+            validator: function(v, callback) {
+              setTimeout(() => {
+                  // do async work.
+                  const result =  v && v.length > 0; // so null doesn't throw validation error...
+                  callback(result);
+              }, 4000);
+            },
+            message: 'a course should have atleast one tag.'
+          }
+        },
+
+  date: {
+          type: Date,
+          default: Date.now
+        },
+
+  isPublished: Boolean,
+
+  price: {
+      type: Number,
+      required: function(){ return this.isPublished;},
+      min: 10,
+      max: 200,
+      get: v => Math.round(v),
+      set: v => Math.round(v)
+      }
+    });
 
 const Course = mongoose.model('Course', courseSchema);
 
@@ -19,74 +59,79 @@ createCourse = async () => {
   try {
     const course = new Course({
       name: 'My Hedge Row Course',
+      category: 'Web',
       author: 's33dco',
-      tags: ['planning', 'law'],
-      isPublished: true
+      tags: ['frontend'],
+      isPublished: true,
+      price: 20
     });
 
     const result = await course.save();
     console.log(result);
   }
   catch (e) {
-    console.log(e.message);
+    for (field in e.errors)
+    console.log(e.errors[field].message);
   }
 }
 
 // createCourse();
 
-getCourses =  async (query) => {
-
-
-
-
-  try {
-    const courses = await Course
-    // comparison
-    // eq ne gt gte lt lte in nin - comparision operators.
-    // .find({price: {$gte :10, $lte: 20}})
-    // .find({price: {$in: [10, 15, 20]}})
-
-    // logical
-    // or and -logical operators
-    // .find()
-    // .or([{author:"tom"}, {author:'frank'}])
-    // .and
-
-    // use regexp
-    // .find({author: /^Car/i}) - starts with Car case insensitve
-    // .find({author: /try$/i}) - sends with try case insensitve
-    // .find({author: /.*hedge.*/i}) - contains hedge case insensitve
-
-    // .count() - to count
-
-      .find(query)
-      .limit(5)
-      .sort({date:1})
-      .select({name:1});
-    console.log(courses);
-  }
-  catch (e) {
-    console.log(e.message);
-  }
-}
-
-getCourses({isPublished: true, tags: 'law'})
+// getCourses =  async (query) => {
+//
+//
+//
+//
+//   try {
+//     const courses = await Course
+//     // comparison
+//     // eq ne gt gte lt lte in nin - comparision operators.
+//     // .find({price: {$gte :10, $lte: 20}})
+//     // .find({price: {$in: [10, 15, 20]}})
+//
+//     // logical
+//     // or and -logical operators
+//     // .find()
+//     // .or([{author:"tom"}, {author:'frank'}])
+//     // .and
+//
+//     // use regexp
+//     // .find({author: /^Car/i}) - starts with Car case insensitve
+//     // .find({author: /try$/i}) - sends with try case insensitve
+//     // .find({author: /.*hedge.*/i}) - contains hedge case insensitve
+//
+//     // .count() - to count
+//
+//       .find(query)
+//       .limit(5)
+//       .sort({date:1})
+//       .select({name:1});
+//     console.log(courses);
+//   }
+//   catch (e) {
+//     console.log(e.message);
+//   }
+// }
+//
+// getCourses({isPublished: true, tags: 'law'})
 
 
 // pagination
 
-paginateCourses = async () => {
+// paginateCourses = async () => {
+//
+//   // /api/courses?pageNumber=2&pageSize=10
+//
+//   const pageNumber = 2;
+//   const pageSize = 10;
+//
+//   const page = await Course
+//     .find({author: 's33dco'})
+//     .skip((pageNumber - 1) * pageSize) //
+//     .limit(pageSize)                   // can retrieve documents in given page
+//     .sort({name: 1})
+// }
+//
+// paginateCourses()
 
-  // /api/courses?pageNumber=2&pageSize=10
-
-  const pageNumber = 2;
-  const pageSize = 10;
-
-  const page = await Course
-    .find({author: 's33dco'})
-    .skip((pageNumber - 1) * pageSize) //
-    .limit(pageSize)                   // can retrieve documents in given page
-    .sort({name: 1})
-}
-
-paginateCourses()
+createCourse()
